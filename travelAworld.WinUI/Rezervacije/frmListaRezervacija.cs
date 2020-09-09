01fedf31f1464b5d6c44e4accd97bd8424bb1d0e
haris babaic <haris.babaic@outlook.com>
2020-09-09 23:56:24 +0200

    omoguceno printanje rezervacija

--- travelAworld.WinUI/Rezervacije/frmListaRezervacija.cs
+++ travelAworld.WinUI/Rezervacije/frmListaRezervacija.cs
@@ -1,13 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using travelAworld.Model;
+using Syncfusion.Pdf;
+using Syncfusion.Pdf.Graphics;
+using Syncfusion.Pdf.Grid;
+
 
 namespace travelAworld.WinUI.Rezervacije
 {
@@ -16,6 +16,7 @@ namespace travelAworld.WinUI.Rezervacije
         private readonly APIService _getPonude = new APIService("ponuda/getponude");
         private readonly APIService _getPonudeUsers = new APIService("ponuda/getponudeusers");
         private int index = 0;
+
         public frmListaRezervacija()
         {
             InitializeComponent();
@@ -33,9 +34,8 @@ namespace travelAworld.WinUI.Rezervacije
             pretragaPonuda.SelectedIndex = index;
 
             txtPageCounter.Text = 1.ToString();
-
         }
-
+ 
         private void frmListaRezervacija_Load(object sender, EventArgs e)
         {
             pretragaRezervacija(sender, e);
@@ -90,6 +90,7 @@ namespace travelAworld.WinUI.Rezervacije
             dgvPonudeUsers.Columns["Jmbg"].Visible = false;
             dgvPonudeUsers.Columns["DatumRodjenja"].Visible = false;
             dgvPonudeUsers.Columns["Id"].Visible = false;
+            dgvPonudeUsers.Columns["DatumPolaska"].Visible = false;
 
         }
         private void dgvPonudeUsers_CellMouseDoubleClick(object sender, DataGridViewCellMouseEventArgs e)
@@ -129,6 +130,101 @@ namespace travelAworld.WinUI.Rezervacije
         private void datumChanged(object sender, EventArgs e)
         {
             pretragaRezervacija(sender, e);
+        }
+
+        private void btnPrintaj_Click(object sender, EventArgs e)
+        {
+            PonudaUserToSearch queryParams = new PonudaUserToSearch
+            {
+                PonudaId = Int32.Parse(pretragaPonuda.SelectedValue.ToString()),
+                Datum = pretragaDatum.Value,
+                PageSize = 5000
+            };
+
+            var result = _getPonudeUsers.Get<PageResult<PonudaUserDisplay>>(queryParams);
+
+            //Create a new PDF document
+            using (PdfDocument doc = new PdfDocument())
+            {
+                //Add a page to the document
+                PdfPage page = doc.Pages.Add();
+
+                //Create a PdfGrid.
+                PdfGrid pdfGrid = new PdfGrid();
+
+                //Create PDF graphics for a page
+                PdfGraphics graphics = page.Graphics;
+
+                //Set the standard font
+                PdfFont font = new PdfStandardFont(PdfFontFamily.Helvetica, 8);
+
+                //Draw the text
+                graphics.DrawString("Rezervacije za mjesec "+ prikaziMjesec(result.Items[0].DatumPolaska.Month), font, PdfBrushes.Black, new PointF(10, 0));
+
+
+                //Create a DataTable.
+                DataTable dataTable = new DataTable();
+                //Add columns to the DataTable
+                dataTable.Columns.Add("#");
+                dataTable.Columns.Add("ID rezervacije");
+                dataTable.Columns.Add("Naziv ponude");
+                dataTable.Columns.Add("Ime i prezime putnika");
+                dataTable.Columns.Add("Uplaćeno");
+                dataTable.Columns.Add("Datum uplate");
+                dataTable.Columns.Add("Tip sobe");
+                dataTable.Columns.Add("Broj osoba");
+                dataTable.Columns.Add("Kontakt telefon");
+                //Add rows to the DataTable.
+                int index = 1;
+                foreach(var rezervacija in result.Items)
+                {
+                    dataTable.Rows.Add(new object[] { index, rezervacija.Id, rezervacija.NazivPonuda, rezervacija.ImePrezime, rezervacija.Cijena+" KM", rezervacija.VrijemePlacanja.ToShortDateString(), rezervacija.TipSobe, rezervacija.BrojOsoba, rezervacija.Telefon != null ? rezervacija.Telefon : "-" });
+                    index++;
+                }
+
+                //Assign data source.
+                pdfGrid.DataSource = dataTable;
+                //Draw grid to the page of PDF document.
+                pdfGrid.Draw(page, new PointF(10, 10));
+                //Save the document.
+                doc.Save("Output.pdf");
+                try
+                {
+                    //Launching the Excel file using the default Application.[MS Excel Or Free ExcelViewer]
+                    System.Diagnostics.Process.Start("Output.pdf");
+
+                    //Exit
+                    Close();
+                }
+                catch (Win32Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                    Close();
+                }
+            }
+
+            string prikaziMjesec(int mjesec)
+            {
+                string mjesecDisplay = null;
+                switch(mjesec)
+                {
+                    case 1: mjesecDisplay = "januar"; break;
+                    case 2: mjesecDisplay = "februar"; break;
+                    case 3: mjesecDisplay = "mart"; break;
+                    case 4: mjesecDisplay = "april"; break;
+                    case 5: mjesecDisplay = "maj"; break;
+                    case 6: mjesecDisplay = "juni"; break;
+                    case 7: mjesecDisplay = "juli"; break;
+                    case 8: mjesecDisplay = "august"; break;
+                    case 9: mjesecDisplay = "septembar"; break;
+                    case 10: mjesecDisplay = "oktobar"; break;
+                    case 11: mjesecDisplay = "novembar"; break;
+                    case 12: mjesecDisplay = "decembar"; break;
+                    default: break;
+                }
+                return mjesecDisplay;
+            }
+
         }
     }
 }
